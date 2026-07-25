dad6d61607af3cd6e785f67e28e360e4b740c02b
mohmmedinham2003-ops <mohmmedinham2003@gmail.com>
2026-07-25 08:59:57 +0530

    Add localStorage operations and include app.js in index.html

---
+++ app.js
@@ -0,0 +1,12 @@
+localStorage.setItem("Name0","bimal");
+
+localStorage.setItem("Name1","kamal");
+
+localStorage.setItem("Name2","nuwan");
+localStorage.setItem("Name3","ranul");
+
+localStorage.removeItem("Name0");
+localStorage.removeItem("Name2");
+
+
+
